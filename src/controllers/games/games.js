1495e117b3b3ef7e3dc8efe3af24659cf559d079
colebeebe <bee14003@byui.edu>
2026-07-02 12:10:39 -0400

    Formatting for game info/review page

--- src/controllers/games/games.js
+++ src/controllers/games/games.js
@@ -3,11 +3,16 @@ import { getGameById } from '../../models/games/games.js';
 
 const router = Router();
 
+router.use('/', (req, res, next) => {
+  res.addStyle('<link rel="stylesheet" href="/css/gameInfo.css" />');
+  next();
+});
+
 router.get('/:id', async (req, res) => {
   const id = req.params.id;
   const game = await getGameById(id);
   res.render('game', {
-    title: 'Game',
+    title: `${game.title} | Oasis Games`,
     game,
   });
 });
